cc2e427d99fefcd96927d7a40dd0cd521b44b985
Jonatan <jonatanblue@users.noreply.github.com>
2023-01-29 21:48:02 +0000

    say it

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import glob
 import sys
+
 from PIL import Image
 from pytesseract import pytesseract
+
+from gtts import gTTS
 
 def see_it(path_to_image):
     img = Image.open(path_to_image)
@@ -11,10 +15,19 @@ def see_it(path_to_image):
     values = text.split(",")
     if len(values) < 2:
         raise RuntimeError(f"Could not identify list of values from text: {values}")
-    print(" ".join(values))
+    return values
+
+def say_it(values):
+    save_directory = "sound_out"
+    for i in range(len(values)):
+        value = values[i]
+        save_path = f"{save_directory}/value_{i}.mp3"
+        tts = gTTS(value, lang='en', slow=True)
+        tts.save(save_path)
+    return save_directory
 
 def main():
-    see_it(sys.argv[1])
+    say_it(see_it(sys.argv[1]))
 
 if __name__ == "__main__":
     main()
